06f4e914c6da18d18c2f3c27d1f6a21ad9c06345
Marco Ledezma Cordero <mledezma@b7-17.local>
2016-11-29 18:59:31 -0600

    More changes yay \!

--- js/main.js
+++ js/main.js
@@ -1,65 +1,80 @@
 $(document).ready(function() {
-    var stock = 0; //Storage the cookies
-    var moreCookies = 0; //Storage the cookies per second
-    var click = 1; //Storage the cookies per click
-    var finishInterval = false; //Finished the Interval
+    var housePrototype = function() {
+        this.stock = 0; //Storage the coins
+        this.moreCoins = 0; //Storage the coins per second
+        this.click = 1; //Storage the coins per click
+        this.upgradeCost1 = 20; //Storage the cost of the upgrade
+        this.upgradeCost2 = 35; //Storage the cost of the upgrade
+        this.finishInterval = false; //Finished the Interval
 
-    // Checks if the stock can enable the upgrades
-    var checkUpgrade = function checkUpgrade() {
-        if (stock >= 20) {
-            $('#upgrade').prop('disabled', false);
-        }
-        else {
-            $('#upgrade').prop('disabled', true);
-        }
+        //Checks if the stock can enable the upgrades
 
-        if (stock >= 35){
-            $('#upgrade2').prop('disabled', false);
-        }
-        else {
-            $('#upgrade2').prop('disabled', true);
-        }
+        this.checkUpgrade = function checkUpgrade() {
+            if (coins.stock >= coins.upgradeCost1) {
+                $('#upgrade1').prop('disabled', false);
+            } else {
+                $('#upgrade1').prop('disabled', true);
+            }
+
+            if (coins.stock >= coins.upgradeCost2) {
+                $('#upgrade2').prop('disabled', false);
+            } else {
+                $('#upgrade2').prop('disabled', true);
+            }
+        };
+
+        // Interval of coinss
+        this.coinsInterval = function() {
+            var coinsIntervalId = setInterval(function() {
+                coins.checkUpgrade();
+                coins.stock += coins.moreCoins;
+                $('#counter').text(coins.stock);
+            }, 1000);
+        };
     };
+    var coins = new housePrototype(); //Creates a new instance of housePrototype
 
-    // Interval of cookies
-    var cookieInterval = function() {
-        var cookieIntervalId = setInterval(function() {
-            checkUpgrade();
-            stock += moreCookies;
-            $('#counter').text(stock);
-        }, 1000);
-    };
+    //Set the initial costs of the upgrades
+    $('#upgradeCost1').text(coins.upgradeCost1);
+    $('#upgradeCost2').text(coins.upgradeCost2);
+    $('#clicks').text(coins.click);
 
     // Check something every time someone moves the mouse
     $(this).on('mousemove', function() {
-        checkUpgrade();
+        coins.checkUpgrade();
     });
 
-    //  Add a cookie per click
-    $('#addCookie').on('click', function() {
-        stock += click;
-        $('#counter').text(stock);
+    //  Add a coins per click
+    $('#addCoins').on('click', function() {
+        coins.stock += coins.click;
+        coins.checkUpgrade();
+        $('#counter').text(coins.stock);
     });
 
-    // Upgrades cookies per second
-    $('#upgrade').on('click', function() {
-        moreCookies += 1;
-        stock -= 20;
-        $('#clickPerSec').text(moreCookies);
-        $('#counter').text(stock);
-
-        // Starts the cookie Interval
-        if (!finishInterval) {
-            cookieInterval();
-        };
-        
-        finishInterval = true;
+    // Upgrades coins per click
+    $('#upgrade1').on('click', function() {
+        coins.click += 1;
+        coins.stock -= coins.upgradeCost1;
+        coins.upgradeCost1 *= 2;
+        $('#upgradeCost1').text(coins.upgradeCost1);
+        $('#counter').text(coins.stock);
+        $('#clicks').text(coins.click);
     });
 
-    // Upgrades cookies per click
+    // Upgrades coins per second
     $('#upgrade2').on('click', function() {
-        stock -= 35;
-        click += 1;
-        $('#counter').text(stock);
+        coins.moreCoins += 1;
+        coins.stock -= coins.upgradeCost2;
+        coins.upgradeCost2 *= 2;
+        $('#upgradeCost2').text(coins.upgradeCost2);
+        $('#clickPerSec').text(coins.moreCoins);
+        $('#counter').text(coins.stock);
+        $('.house').append('<div class="sofa"></div>');
+
+        // Starts the coins Interval
+        if (!coins.finishInterval) {
+            coins.coinsInterval();
+            coins.finishInterval = true;
+        };
     });
 });
